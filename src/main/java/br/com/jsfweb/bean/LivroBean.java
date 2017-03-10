8457aaaef46936ec7e3885560070467e2bbb93bc
Hugo <hugo.nobreg@gmail.com>
2017-03-09 22:42:49 -0300

    Exercícios

--- src/main/java/br/com/jsfweb/bean/LivroBean.java
+++ src/main/java/br/com/jsfweb/bean/LivroBean.java
@@ -2,8 +2,12 @@ package br.com.jsfweb.bean;
 
 import java.util.List;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.ValidatorException;
 
 import br.com.jsfweb.dao.DAO;
 import br.com.jsfweb.model.Autor;
@@ -20,7 +24,7 @@ public class LivroBean {
 		System.out.println("Gravando livro " + livro.getTitulo());
 
 		if (livro.getAutores().isEmpty()) {
-			throw new RuntimeException("Livro deve ter pelo menos um Autor");
+			FacesContext.getCurrentInstance().addMessage("autor", new FacesMessage("Livro deve ter pelo menos um Autor"));
 		}
 
 		new DAO<Livro>(Livro.class).adiciona(this.livro);
@@ -30,6 +34,13 @@ public class LivroBean {
 		Autor autor = new DAO<Autor>(Autor.class).buscaPorId(this.autorId);
 		this.livro.adicionaAutor(autor);
 		System.out.println("Livro gravado por:" + autor.getNome());
+	}
+
+	public void comecaComDigitoUm(FacesContext fc, UIComponent component, Object value) throws ValidatorException{
+		String valor = value.toString();
+		if(!valor.startsWith("1")){
+			throw new ValidatorException(new FacesMessage("ISBN - Deveria começar com 1"));
+		}
 	}
 
 	public Livro getLivro() {
