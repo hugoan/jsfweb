79721c80425b974e9dde8f1d23916963d4ae519b
Hugo <hugo.nobreg@gmail.com>
2017-03-18 22:47:50 -0300

    Exercícios

--- src/main/java/br/com/jsfweb/bean/loginBean.java
+++ src/main/java/br/com/jsfweb/bean/loginBean.java
@@ -1,7 +1,9 @@
 package br.com.jsfweb.bean;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
 
 import br.com.jsfweb.dao.UsuarioDAO;
 import br.com.jsfweb.model.Usuario;
@@ -19,12 +21,23 @@ public class loginBean {
 	public String efetuaLogin() {
 		System.out.println("Fazendo login do usuário " + this.usuario.getEmail());
 
+		FacesContext context = FacesContext.getCurrentInstance();
 		boolean existe = new UsuarioDAO().existe(this.usuario);
 
 		if (existe) {
+			context.getExternalContext().getSessionMap().put("usuarioLogado", this.usuario);
 			return "livro?faces-redirect=true";
 		}
-		return null;
+		
+		context.getExternalContext().getFlash().setKeepMessages(true);
+		context.addMessage(null, new FacesMessage("Usuário não encontrado"));
+		return "login?faces-redirect=true";
+	}
+
+	public String deslogar() {
+		FacesContext context = FacesContext.getCurrentInstance();
+		context.getExternalContext().getSessionMap().remove("usuarioLogado");
+		return "login?faces-redirect=true";
 	}
 
 }
